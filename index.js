b54d6ad0d8b3543198291b66e8684f0cb57bdbaa
Michael Giuliana <michael.giuliana@gmail.com>
2017-03-20 16:44:29 -0400

    add static server

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config({ silent: true });
+
+var express = require('express');
+var resolve = require('path').resolve;
+var basicAuth = require('express-basic-auth');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static('build'));
+
+app.use(basicAuth({
+  challenge: true,
+  realm: 'JAKO Documentation',
+  authorizer: function (username, password) {
+    var key = 'BASIC_AUTH_' + username;
+    var envUser = process.env['BASIC_AUTH_' + username];
+
+    if (!(key in process.env)
+        || process.env[key] !== password
+    ) {
+      console.log('login attempt by ' + key + ' failed');
+      return false;
+    }
+
+    return true;
+  }
+}));
+
+app.get('/', function (req, res) {
+  res.sendFile(resolve(__dirname + '/build/index.html'));
+});
+
+app.get('*', function (req, res) {
+  res.redirect('/');
+});
+
+app.listen(port);
